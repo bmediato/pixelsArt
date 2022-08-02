99dada465606ccbdf2892545c209aa6bf0470773
Beatriz <beatriz.mediatto2@gmail.com>
2022-08-02 19:27:43 -0300

    tentando resovler o sexto requisito

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+function criandoFilho () {
 const pai = document.querySelector('#pixel-board');
 
 for (let index = 1; index <= 25; index += 1) {
@@ -5,3 +7,16 @@ for (let index = 1; index <= 25; index += 1) {
   filho.className = 'pixel';
   pai.appendChild(filho);
 }
+}
+criandoFilho ();
+
+const corPreta = document.getElementsByClassName('preto')[0];
+const corRosa = document.getElementsByClassName('rosa')[0];
+const corRoxo = document.getElementsByClassName('roxo')[0];
+const corAzul = document.getElementsByClassName('azul')[0];
+
+window.onload = corPreta.addEventListener('click', function () {
+    corPreta.classList.remove('color');
+    corPreta.classList.add('selected');
+    corPreta.classList.add('color');
+})
