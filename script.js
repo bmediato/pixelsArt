e38296f474d3fbd0296abe9b5f1c99482f09c973
Beatriz <beatriz.mediatto2@gmail.com>
2022-08-02 18:52:44 -0300

    ajustando

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-let pai = document.querySelector('#pixel-board');
+const pai = document.querySelector('#pixel-board');
 
-for (let index = 1; index <= 25; index +=1) {
-    let filho = document.createElement('div');
-    filho.className = 'pixel';
-    pai.appendChild(filho);
+for (let index = 1; index <= 25; index += 1) {
+  const filho = document.createElement('div');
+  filho.className = 'pixel';
+  pai.appendChild(filho);
 }
-
-
