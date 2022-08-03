5aafa9133d43e245cac78528e7ca0492f8cc2e5a
Beatriz <beatriz.mediatto2@gmail.com>
2022-08-03 19:37:49 -0300

    Primeira parte do requisito sete, nem acredito

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
-function criandoFilho () {
-const pai = document.querySelector('#pixel-board');
+function criandoFilho() {
+  const pai = document.querySelector('#pixel-board');
 
-for (let index = 1; index <= 25; index += 1) {
-  const filho = document.createElement('div');
-  filho.className = 'pixel';
-  pai.appendChild(filho);
+  for (let index = 1; index <= 25; index += 1) {
+    const filho = document.createElement('div');
+    filho.className = 'pixel';
+    pai.appendChild(filho);
+  }
 }
-}
-criandoFilho ();
+criandoFilho();
+
+const corPreta = document.getElementById('preto');
+const corRosa = document.getElementById('rosa');
+const corRoxo = document.getElementById('roxo');
+const corAzul = document.getElementById('azul');
 
 window.onload = function () {
-  const corPreta = document.getElementById('preto');
   corPreta.classList.remove('color');
   corPreta.classList.add('selected');
   corPreta.classList.add('color');
 }
 
+const cores = document.querySelectorAll('.color');
+
+function selectColor(evento) {
+  for (let index = 0; index < cores.length; index += 1) {
+    cores[index].classList.remove('selected');
+  }
+  evento.target.classList.add('selected');
+}
+
+for (let index = 0; index < cores.length; index += 1) {
+  cores[index].addEventListener('click', selectColor);
+}
